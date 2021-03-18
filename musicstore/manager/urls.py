90a7cbc3273f9bebab5a3aacec4a1dd400f6ab2c
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 15:44:01 -0300

    urls para tela de detalhes

--- musicstore/manager/urls.py
+++ musicstore/manager/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from .views import (
+    MusicDetailView,
     MusicListView,
     MusicCreateView,
     MusicUpdateView,
@@ -11,6 +12,7 @@ app_name = 'manager'
 urlpatterns = [
     path('', MusicListView.as_view(), name="music-list"),
     path('create/', MusicCreateView.as_view(), name="music-create"),
+    path('detail/<int:pk>', MusicDetailView.as_view(), name="music-detail"),
     path('update/<int:pk>', MusicUpdateView.as_view(), name="music-update"),
     path('delete/<int:pk>', MusicDeleteView.as_view(), name="music-delete"),
 ]
