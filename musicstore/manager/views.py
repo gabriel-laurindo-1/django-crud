27dee674b3b976578f25086d4bfb9149db43c0a3
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 10:00:16 -0300

    Criação da views para o app manager

--- musicstore/manager/views.py
+++ musicstore/manager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+
+from .models import *
+from .forms import MusicModelForm
 
 # Create your views here.
+class MusicCreateView(CreateView):
+    
+    model = MUSIC_MODEL
+    template_name = 'home.html'
+    fields = '__all__'
+    
