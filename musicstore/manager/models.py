dcde0da9b08a0502f2c0f81f23ef692623431ea6
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 09:59:53 -0300

    Criação do models para o app manager

--- musicstore/manager/models.py
+++ musicstore/manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MUSIC_MODEL(models.Model):
+    name = models.CharField(max_length=50, verbose_name='name')
+    year = models.IntegerField()
+    description = models.CharField(max_length=50, verbose_name='Description')
+
+    def __str__(self):
+        return f"{self.name}[{self.year}]: {self.description}"
+
