b1f8d30db8d5b301fd97d5b61517a08e82aca5ef
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 16:03:43 -0300

    Nome do Model customizado

--- musicstore/manager/models.py
+++ musicstore/manager/models.py
@@ -4,9 +4,11 @@ from django.utils.timezone import now
 class MUSIC_MODEL(models.Model):
     name = models.CharField(max_length=50, verbose_name='name')
     year = models.IntegerField()
-    description = models.CharField(max_length=50, null=True, verbose_name='Description')
+    description = models.CharField(max_length=50, null=True, blank=True, verbose_name='Description')
     last_modification = models.DateTimeField(auto_now=True, null=True, verbose_name='Date Last Modification')
 
     def __str__(self):
         return f"{self.name}[{self.year}]: {self.description}"
 
+    class Meta:
+        db_table = 'MUSIC_MODEL'
