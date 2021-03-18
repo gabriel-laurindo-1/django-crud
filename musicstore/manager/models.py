067dcd07362dcd5bfb6c6103a187c491a4e6ae9a
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 15:43:15 -0300

    Adição de data de ultima modificação

--- musicstore/manager/models.py
+++ musicstore/manager/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
 class MUSIC_MODEL(models.Model):
     name = models.CharField(max_length=50, verbose_name='name')
     year = models.IntegerField()
-    description = models.CharField(max_length=50, verbose_name='Description')
+    description = models.CharField(max_length=50, null=True, verbose_name='Description')
+    last_modification = models.DateTimeField(auto_now=True, null=True, verbose_name='Date Last Modification')
 
     def __str__(self):
         return f"{self.name}[{self.year}]: {self.description}"
