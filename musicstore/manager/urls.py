3ccd6facd41eb83e21ff6d6bafc0eabf2417ab58
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-18 13:40:44 -0300

    urls para lista, update e delete

--- musicstore/manager/urls.py
+++ musicstore/manager/urls.py
@@ -1,8 +1,16 @@
 
 from django.urls import path
-from .views import MusicCreateView
+from .views import (
+    MusicListView,
+    MusicCreateView,
+    MusicUpdateView,
+    MusicDeleteView
+)
 
 app_name = 'manager'
 urlpatterns = [
+    path('', MusicListView.as_view(), name="music-list"),
     path('create/', MusicCreateView.as_view(), name="music-create"),
+    path('update/<int:pk>', MusicUpdateView.as_view(), name="music-update"),
+    path('delete/<int:pk>', MusicDeleteView.as_view(), name="music-delete"),
 ]
