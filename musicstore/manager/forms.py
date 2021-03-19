5acd397ee1e0fd0879ddddc1203db966d3c324ff
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-03-19 11:09:48 -0300

    Novos formularios

--- musicstore/manager/forms.py
+++ musicstore/manager/forms.py
@@ -6,3 +6,13 @@ class MusicModelForm(forms.ModelForm):
     class Meta:
         model = MUSIC_MODEL
         fields = '__all__'
+
+class GenreModelForm(forms.ModelForm):
+    class Meta:
+        model = MUSIC_GENRE_MODEL
+        fields = '__all__'
+
+class ArtistModelForm(forms.ModelForm):
+    class Meta:
+        model = ARTIST_MODEL
+        fields = '__all__'
