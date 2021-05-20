119d6495f2e2fcb9639a1d6de5e65556bdc70227
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2021-05-20 08:01:29 -0300

    AlbumWizard SessionWizardView

--- musicstore/manager/views.py
+++ musicstore/manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import (
     DetailView, 
@@ -7,7 +8,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
-
+from formtools.wizard.views import SessionWizardView
 
 from .models import *
 from .forms import *
@@ -153,3 +154,43 @@ class ArtistDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse('manager:artist-list')
+
+
+def process_form_data(form_list):
+    form_data = [form.cleaned_data for form in form_list]
+    return form_data
+
+def show_message_form_condition(wizard):
+    # try to get the cleaned data of step 1
+    cleaned_data = wizard.get_cleaned_data_for_step('0') or {}
+    # check if the field ``leave_message`` was checked.
+    print('show_message_form_condition:', cleaned_data.get('production_year', True))
+    return cleaned_data.get('production_year', True)
+
+class AlbumWizard(SessionWizardView):
+    template_name = 'contact.html'
+    success_url = 'contact.html'
+    form_list = [AlbumForm1, AlbumForm2, AlbumForm3]
+
+
+    def get_form_step_data(self, form):
+        return form.data
+
+    def get_context_data(self, form, **kwargs):
+        context = super().get_context_data(form=form, **kwargs)
+        context['state_form'] = 'Welcome'
+
+        # print('self.steps.current:', self.steps.current, type(self.steps.current))
+
+        if self.steps.current == '1':
+            context.update(
+                {
+                    'state_form': 'WVT'
+                })
+        return context
+
+    def done(self, form_list, **kwargs):
+        form_data = process_form_data(form_list)
+
+        return render(self.request, template_name='done.html', context={'form_data': form_data})
+
